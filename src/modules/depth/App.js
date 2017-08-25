3467bd01cf913991471e0d026f41b8dc001ae4e7
dllo <dllo@doubidaibiao.local>
2017-08-25 17:18:19 +0800

    fix bugs

---
+++ src/modules/depth/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import '../../assets/styles/depth.styl'
+import Read from '../../components/depth-recommend-read'
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <Read />
+            </div>
+        )
+    }
+}
+export default App
--- src/modules/index/App.js
+++ src/modules/index/App.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react'
-import '../../assets/styles/depth.styl'
-import Read from '../../components/depth-recommend-read'
 import '../../assets/styles/Homeright.styl'
 import HomeRight from '../../component/homeright'
 import '../../assets/styles/App.styl'
@@ -11,7 +9,7 @@ class App extends Component {
   render () {
     return (
       <div>
-        <Read />
+        <a href='depth.html'>depth</a>
         <Home />
         <HomeRight />
       </div>
