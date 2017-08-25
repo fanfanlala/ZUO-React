422d1a39fcd0e6cbdf89eefd5098efdb5b1d550b
康俊鹏 <kangjunpeng@kangjunpengdeMacBook-Pro.local>
2017-08-25 15:09:02 +0800

    second

--- src/modules/index/App.js
+++ src/modules/index/App.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react'
-import '../../assets/styles/depth.styl'
-import Read from '../../components/depth-recommend-read'
 import '../../assets/styles/Homeright.styl'
 import '../../assets/styles/App.styl'
 import '../../assets/styles/Home.styl'
@@ -10,7 +8,7 @@ class App extends Component {
   render () {
     return (
       <div>
-        <Read />
+        <a href='depth.html'>点我跳转</a>
         <Home />
       </div>
     )
